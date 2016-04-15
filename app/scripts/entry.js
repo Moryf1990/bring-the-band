7fadb55a5ea70d8bbbe3562cc7ba5374413a898a
Mory Friedman <friedmanmory2@gmail.com>
2016-04-15 13:59:58 -0500

    creating nav component

---
+++ app/scripts/entry.js
@@ -0,0 +1,68 @@
+// import the stylesheet. this is necessary so that webpack will compile all the sass into css and then build it into our style.css file
+import './../styles/main.scss';
+
+// import a module from another file.
+import React from 'react';
+import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
+import Nav from './nav.js';
+import {Router, Route, hashHistory, Link} from 'react-router'
+// import App from './app.js';
+
+// render(
+//   <App/>,
+//   document.getElementById('app')
+// );
+
+const Home = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<Nav/>
+				<h1>Welcome to the Bands</h1>
+			</div>
+		)
+	}
+});
+
+const Search = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<Nav/>
+				<h1>Search Here</h1>
+				<input type="text" placeholder="Enter Band"/>
+				<button>Search</button>
+			</div>
+		);
+	}
+});
+
+const Vote = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<Nav/>
+				<h1>Here is where you vote</h1>
+			</div>
+		);
+	}
+});
+
+const router = (
+	<Router history = {hashHistory}>
+		<Route path = "/" component={Home}/>
+		<Route path = "/search" component={Search}/>
+		<Route path = "/vote" component={Vote}/>
+	</Router>
+)
+
+ReactDOM.render(router, document.getElementById('app'));
+
+
+
+
+
+
+
+
---
+++ app/scripts/nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+const Nav = React.createClass({
+	render: function() {
+		return (
+			<nav>
+				<Link to = "/">Home</Link>
+				<Link to = "/search">Search</Link>
+				<Link to = "/vote">Vote</Link>
+			</nav>
+		);
+	}
+});
+
+export default Nav;
